4d4c2e4dd59dfbe99d93d8e83ad2d27659cc3e5e
claire <evil10685@gmail.com>
2022-09-03 14:59:12 +0800

    feat:2022_0903

--- 1-7-2-animate/js/index.js
+++ 1-7-2-animate/js/index.js
@@ -1,7 +1,14 @@
+const duration = 500;
 
 $('#BtnRight').click(function () {
+    $('#Rocket').stop(true, false).animate({
+        left: '+=200'
+    }, duration, 'easeInBounce')
 });
 
 $('#BtnLeft').click(function () {
+    $('#Rocket').stop(true, false).animate({
+        left: '-=200'
+    }, duration, 'easeInBounce')
 });
 
